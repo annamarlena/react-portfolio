6cd8060e2287fbc295f4f5fede97d0ed62f0c793
Anna Marlena <annamarlena310@gmail.com>
2023-05-29 20:54:47 -0500

    Started working on portfolio

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+// import './header.css'; <--- to be created later
+
+function Header() {
+  return(
+    <div>
+        <h1>A. Marlena Keller - a Portfolio</h1>
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+
+// This is where we're telling React to go get the React application from the index.html file
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
