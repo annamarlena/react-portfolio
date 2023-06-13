748be69a85942f8fdcdeee5a479f590ba01de9f0
Anna Marlena <annamarlena310@gmail.com>
2023-06-13 14:09:24 -0500

    Added hamburger menu to navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,12 +15,15 @@ function Header({currentPage}) {
 
         <Navbar bg="dark" variant="dark" expand="sm">
           <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="nav-link">
               <NavLink className={currentPage === "/react-portfolio/about" ? "active-link" : ""} href="/react-portfolio/about">About</NavLink>
               <NavLink className={currentPage === "/react-portfolio/portfolio" ? "active-link" : ""} href="/react-portfolio/portfolio">Portfolio</NavLink>
               <NavLink className={currentPage === "/react-portfolio/resume" ? "active-link" : ""} href="/react-portfolio/resume">Resume</NavLink>
               <NavLink className={currentPage === "/react-portfolio/contact" ? "active-link" : ""} href="/react-portfolio/contact">Contact</NavLink>
             </Nav>
+            </Navbar.Collapse>
           </Container>
         </Navbar>
       </div>
